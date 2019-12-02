545af35fa84068235060608ffd69e6f7e9076aa2
fan-jia <fanjia1996@gmail.com>
2019-12-02 08:50:35 -0500

    Unifinished Code

--- planner/rrt_planner.py
+++ planner/rrt_planner.py
@@ -1,12 +1,48 @@
 import numpy as np
+import enum
+import math
 
-def planner (env,start,goal):
+#math.pi
+
+class Node:
+	def __init__(self, numofDOFs):
+		self.arm_anglesV_rad = np.zeros(numofDOFs)
+
+class ExtendStatus(enum.Enum):
+	REACHED = 1
+	ADVANCED = 2
+	TRAPPED = 3
+
+def ContXY2Cell(x, y, pX, pY, x_size, y_size):
+	cellsize = 1.0
+	pX = int(x/cellsize)
+	if x < 0:
+		pX = 0
+	if pX >= x_size:
+		pX = x_size - 1
+
+	pY = int(y/cellsize)
+	if y < 0:
+		pY = 0
+	if pY >= y_size:
+		pY = y_size - 1
+
+	return pX, pY
+
+def getRandomNode()
+
+
+
+
+
+
+
+
+def planner(env,start,goal):
 	print(env[0])
 	map = np.loadtxt(env)
-	a = [2,2]
-	b = [1,1]
+	print('start is ', start)
 
-	return a,b
 
 
 
